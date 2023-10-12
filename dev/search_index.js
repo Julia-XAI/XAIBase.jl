00b6bc883fc19dfc6576d46820818ea633dd7dc9
Documenter.jl <documenter@juliadocs.github.io>
2023-10-12 17:11:28 +0000

    build based on c1659ff

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = XAIBase","category":"page"},{"location":"#XAIBase","page":"Home","title":"XAIBase","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for XAIBase.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [XAIBase]","category":"page"}]
+}
