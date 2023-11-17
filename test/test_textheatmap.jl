3641cc24afe64869b75340aa839b664991df4c68
Adrian Hill <adrian.hill@mailbox.org>
2023-11-17 16:23:24 +0100

    Update tests

--- test/test_textheatmap.jl
+++ test/test_textheatmap.jl
@@ -1,22 +1,20 @@
-using TextHeatmaps
-
 val = output = [1 6; 2 5; 3 4]
 text = [["Test", "Text", "Heatmap"], ["another", "dummy", "input"]]
 neuron_selection = [CartesianIndex(1, 2), CartesianIndex(3, 4)] # irrelevant
 expl = Explanation(val, output, neuron_selection, :Gradient)
-h = TextHeatmaps.heatmap(expl, text)
+h = textheatmap(expl, text)
 @test_reference "references/Gradient1.txt" repr("text/plain", h[1])
 @test_reference "references/Gradient2.txt" repr("text/plain", h[2])
 
 expl = Explanation(val[:, 1:1], output[:, 1:1], neuron_selection[1], :Gradient)
-h = TextHeatmaps.heatmap(expl, text[1])
-@test_reference "references/Gradient1.txt" repr("text/plain", h)
+h = textheatmap(expl, text[1])
+@test_reference "references/Gradient1.txt" repr("text/plain", only(h))
 
 expl = Explanation(val, output, neuron_selection, :LRP)
-h = TextHeatmaps.heatmap(expl, text)
+h = textheatmap(expl, text)
 @test_reference "references/LRP1.txt" repr("text/plain", h[1])
 @test_reference "references/LRP2.txt" repr("text/plain", h[2])
 
-h = TextHeatmaps.heatmap(expl, text; rangescale=:extrema)
+h = textheatmap(expl, text; rangescale=:extrema)
 @test_reference "references/LRP1_extrema.txt" repr("text/plain", h[1])
 @test_reference "references/LRP2_extrema.txt" repr("text/plain", h[2])
