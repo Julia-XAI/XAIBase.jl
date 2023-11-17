cfa1c70424eaccb3b93015e9fbd87d25a5a69e1a
Adrian Hill <adrian.hill@mailbox.org>
2023-11-17 15:22:47 +0100

    Update Aqua.jl tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,9 +6,7 @@ using Aqua
 @testset "XAIBase.jl" begin
     @testset "Aqua.jl" begin
         @info "Running Aqua.jl's auto quality assurance tests. These might print warnings from dependencies."
-        # Package extensions break Project.toml formatting tests on Julia 1.6
-        # https://github.com/JuliaTesting/Aqua.jl/issues/105
-        Aqua.test_all(XAIBase; ambiguities=false, project_toml_formatting=VERSION >= v"1.7")
+        Aqua.test_all(XAIBase; ambiguities=false)
     end
     @testset "API" begin
         @info "Testing API..."
