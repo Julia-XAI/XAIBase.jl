5ea16226dbbf5d2a61fe6ccf9a6259dfc0f20369
Adrian Hill <adrian.hill@mailbox.org>
2024-02-07 17:36:13 +0100

    Increase code coverage
    by removing unused Base.show method

--- src/compat.jl
+++ src/compat.jl
@@ -7,12 +7,4 @@ if VERSION < v"1.7.0-DEV.793"
         Returns{V}(value) where {V} = new{V}(value)
         Returns(value) = new{Core.Typeof(value)}(value)
     end
-
-    (obj::Returns)(args...; kw...) = obj.value
-    function Base.show(io::IO, obj::Returns)
-        show(io, typeof(obj))
-        print(io, "(")
-        show(io, obj.value)
-        return print(io, ")")
-    end
 end
