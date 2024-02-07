cbac95df09953e485ab0089d32aca16bb5b19452
Adrian Hill <adrian.hill@mailbox.org>
2024-02-07 17:54:23 +0100

    Fix for Julia 1.6

--- src/compat.jl
+++ src/compat.jl
@@ -7,4 +7,6 @@ if VERSION < v"1.7.0-DEV.793"
         Returns{V}(value) where {V} = new{V}(value)
         Returns(value) = new{Core.Typeof(value)}(value)
     end
+
+    (obj::Returns)(args...; kw...) = obj.value
 end
