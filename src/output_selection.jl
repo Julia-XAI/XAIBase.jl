a23d0c7e592a764517d14af6ba3aa2d54a7fdf51
Adrian Hill <adrian.hill@mailbox.org>
2024-02-14 17:10:32 +0100

    Improve output selector doc strings

--- src/output_selection.jl
+++ src/output_selection.jl
@@ -1,9 +1,40 @@
+"""
+Abstract super type of all output selectors in XAIBase.
+
+Output selectors are expected to be callable and to return a vector of `CartesianIndex`
+of the selected outputs.
+
+## Note
+XAIBase assumes that the batch dimension is the last dimension of the output.
+"""
 abstract type AbstractOutputSelector end
 
 """
     MaxActivationSelector()
 
 Output selector that picks the output with the highest activation.
+
+## Note
+XAIBase assumes that the batch dimension is the last dimension of the output.
+
+## Example
+```julia-repl
+julia> output = rand(3, 3)
+3×3 Matrix{Float64}:
+ 0.411871  0.313366  0.13402
+ 0.885562  0.136938  0.465622
+ 0.498235  0.627209  0.298911
+
+julia> output_selector = MaxActivationSelector()
+ MaxActivationSelector()
+
+julia> output_selector(output)
+ 3-element Vector{CartesianIndex{2}}:
+  CartesianIndex(2, 1)
+  CartesianIndex(3, 2)
+  CartesianIndex(2, 3)
+```
+
 """
 struct MaxActivationSelector <: AbstractOutputSelector end
 function (::MaxActivationSelector)(out::AbstractArray{T,N}) where {T,N}
@@ -15,6 +46,29 @@ end
     IndexSelector(index)
 
 Output selector that picks the output at the given index.
+
+## Note
+XAIBase assumes that the batch dimension is the last dimension of the output.
+
+## Example
+```julia-repl
+julia> output = rand(3, 3)
+3×3 Matrix{Float64}:
+ 0.411871  0.313366  0.13402
+ 0.885562  0.136938  0.465622
+ 0.498235  0.627209  0.298911
+
+julia> output_selector = IndexSelector(1)
+ IndexSelector{Int64}(1)
+
+julia> output_selector(output)
+ 3-element Vector{CartesianIndex{2}}:
+  CartesianIndex(1, 1)
+  CartesianIndex(1, 2)
+  CartesianIndex(1, 3)
+```
+
+Note that XAIBase's output selectors assume that the batch dimension comes last.
 """
 struct IndexSelector{I} <: AbstractOutputSelector
     index::I
